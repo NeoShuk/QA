b34163d4e3609259001338ff4104b52f1225681f
NeoShuk <neoweek@yandex.ru>
2024-05-25 22:35:47 +0300

    Homework2

--- src/Main.java
+++ src/Main.java
@@ -1,160 +1,30 @@
 public class Main {
     public static void main(String[] args) {
-        printThreeWords();
-        checkSumSign();
-        printColor();
-        compareNumbers();
-        checkSumInRange(5, 20);
-        checkPositiveOrNegative(-10);
-        isNegative(-10);
-        printString("Hello, Aston!", 3);
-
-        System.out.println(isLeapYear(2000));
-
-        arrayInverter();
-        arrayFiller();
-        multiplyLessThanSix();
-        matrix();
-        createArrayWithInitialValue(5, 10);
-    }
-
-    public static void printThreeWords() {
-        System.out.println("Orange");
-        System.out.println("Banana");
-        System.out.println("Apple");
-    }
-
-    public static void checkSumSign() {
-        int a = 10;
-        int b = -20;
-
-        int sum = a + b;
-
-        if (sum >= 0) {
-            System.out.println("Сумма положительная");
-        } else {
-            System.out.println("Сумма отрицательная");
-        }
-    }
-
-    public static void printColor() {
-        int value = 0;
-
-        if (value <= 0) {
-            System.out.println("Красный");
-        } else if (value > 0 && value <= 100) {
-            System.out.println("Желтый");
-        } else {
-            System.out.println("Зеленый");
-        }
-    }
-
-    public static void compareNumbers() {
-        int a = 10;
-        int b = 5;
-
-        if (a >= b) {
-            System.out.println("a >= b");
-        } else {
-            System.out.println("a < b");
-        }
-    }
-
-    public static boolean checkSumInRange(int a, int b) {
-        int sum = a + b;
-        boolean result = sum >= 10 && sum <= 20;
-        System.out.println(result);
-        return result;
-    }
-
-    public static void checkPositiveOrNegative(int number) {
-        if (number >= 0) {
-            System.out.println(number + " - положительное число");
-        } else {
-            System.out.println(number + " - отрицательное число");
-        }
-    }
-
-    public static boolean isNegative(int number) {
-        boolean result = number < 0;
-        System.out.println(result);
-        return result;
-    }
-
-    public static void printString(String str, int count) {
-        for (int i = 0; i < count; i++) {
-            System.out.println(str);
-        }
-    }
-
-    public static boolean isLeapYear(int year) {
-        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
-    }
-
-    public static void arrayInverter() {
-        int[] array = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
-
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] == 0) {
-                array[i] = 1;
-            } else {
-                array[i] = 0;
-            }
-        }
-        for (int value : array) {
-            System.out.print(value + " ");
-        }
+        Employee employeeTest = new Employee("Ivan Ivanov", "Engineer", "ivanov@example.com", "123-456-7890", 50000, 30);
+        employeeTest.printInfo();
         System.out.println();
-    }
 
-    public static void arrayFiller() {
-        int[] array = new int[100];
+        Employee[] employees = new Employee[5];
+        employees[0] = new Employee("Ivanov Sergey", "Engineer", "ivivan@mailbox.com", "892312312", 30000, 30);
+        employees[1] = new Employee("Petrov Petr", "Manager", "petrov@mailbox.com", "892312313", 40000, 40);
+        employees[2] = new Employee("Sidorov Ivan", "Director", "sidorov@mailbox.com", "892312314", 50000, 50);
+        employees[3] = new Employee("Smirnov Ivan", "Developer", "smirnov@mailbox.com", "892312315", 35000, 28);
+        employees[4] = new Employee("Kuznetsov Alexey", "Analyst", "kuznetsov@mailbox.com", "892312316", 45000, 35);
 
-        for (int i = 0; i < array.length; i++) {
-            array[i] = i + 1;
-        }
-        for (int value : array) {
-            System.out.print(value + " ");
-        }
-        System.out.println();
-    }
-
-    public static void multiplyLessThanSix() {
-        int[] array = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
-
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] < 6) {
-                array[i] *= 2;
-            }
-        }
-        for (int value : array) {
-            System.out.print(value + " ");
-        }
-    }
-
-    public static void matrix() {
-        int size = 5;
-        int[][] matrix = new int[size][size];
-
-        for (int i = 0; i < size; i++) {
-            matrix[i][i] = 1;
-        }
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                System.out.print(matrix[i][j] + " ");
-            }
+        for (Employee employee : employees) {
+            employee.printInfo();
             System.out.println();
         }
-    }
 
-    public static int[] createArrayWithInitialValue(int len, int initialValue) {
-        int[] array = new int[len];
-        for (int i = 0; i < len; i++) {
-            array[i] = initialValue;
+        Park park = new Park();
+        Park.Attraction[] attractions = new Park.Attraction[3];
+        attractions[0] = park.new Attraction("Roller Coaster", "10:00 - 18:00", 5.0);
+        attractions[1] = park.new Attraction("Ferris Wheel", "09:00 - 20:00", 3.0);
+        attractions[2] = park.new Attraction("Haunted House", "11:00 - 22:00", 6.0);
+
+        for (Park.Attraction attraction : attractions) {
+            attraction.printInfo();
+            System.out.println();
         }
-        for (int value : array) {
-            System.out.print(value + " ");
-        }
-        return array;
     }
 }
