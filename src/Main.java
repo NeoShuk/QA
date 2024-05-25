caa24b83886cd708f5d58b6261b398a0d5513954
NeoShuk <166657186+NeoShuk@users.noreply.github.com>
2024-05-25 22:52:02 +0300

    Nothing special

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        Employee employeeTest = new Employee("Ivan Ivanov", "Engineer", "ivanov@example.com", "123-456-7890", 50000, 30);
+        Employee employeeTest = new Employee("Ivan Ivanov", "AQA", "ivanov@yandex.ru", "89585347064", 50000, 24);
         employeeTest.printInfo();
         System.out.println();
 
         Employee[] employees = new Employee[5];
-        employees[0] = new Employee("Ivanov Sergey", "Engineer", "ivivan@mailbox.com", "892312312", 30000, 30);
-        employees[1] = new Employee("Petrov Petr", "Manager", "petrov@mailbox.com", "892312313", 40000, 40);
-        employees[2] = new Employee("Sidorov Ivan", "Director", "sidorov@mailbox.com", "892312314", 50000, 50);
-        employees[3] = new Employee("Smirnov Ivan", "Developer", "smirnov@mailbox.com", "892312315", 35000, 28);
-        employees[4] = new Employee("Kuznetsov Alexey", "Analyst", "kuznetsov@mailbox.com", "892312316", 45000, 35);
+        employees[0] = new Employee("Ivanov Sergey", "Engineer", "ivivan123@mail.com", "89509404929", 30000, 31);
+        employees[1] = new Employee("Petrov Petr", "Manager", "petrovvvvv@gmail.com", "89014765294", 40000, 45);
+        employees[2] = new Employee("Sidorov Ivan", "Director", "sidorov_ivanchela@yahoo.com", "89945578995", 50000, 52);
+        employees[3] = new Employee("Smirnov Ivan", "Developer", "smirnov234@mail.com", "89580393053", 35000, 28);
+        employees[4] = new Employee("Kuznetsov Alexey", "Analyst", "kuznetsov9057@yandex.com", "89018769581", 45000, 35);
 
         for (Employee employee : employees) {
             employee.printInfo();
