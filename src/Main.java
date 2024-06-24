b6daf9451b1f8ddd9b9bad5857258934c2777e60
Neoweek <neoweek@yandex.ru>
2024-06-24 19:55:40 +0300

    HomeWork

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,28 @@
-import java.util.Map;
-import java.util.Set;
-
 public class Main {
     public static void main(String[] args) {
-        String[] words = {"Гном", "Дворф", "Гном", "Орк", "Дворф", "Тролль", "Гном", "Тролль", "Человек", "Отрекшийся", "Отрекшийся", "Человек", "Гном", "Эльф", "Орк"};
-        UniqueWords uniqueWords = new UniqueWords(words);
-
-        Set<String> uniqueWordsSet = uniqueWords.getUniqueWords();
-        System.out.println("Список уникальных слов: " + uniqueWordsSet);
-
-        Map<String, Integer> wordCountMap = uniqueWords.getWordCount();
-        System.out.println("Количество вхождений каждого слова:");
-        for (Map.Entry<String, Integer> entry : wordCountMap.entrySet()) {
-            System.out.println(entry.getKey() + ": " + entry.getValue());
+        try {
+            System.out.println("0! =  " + Factorial.compute(0));
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
         }
 
-        PhoneBook phoneBook = new PhoneBook();
-        phoneBook.add("Karpacheva", "89653779051");
-        phoneBook.add("Stepankov", "89771676485");
-        phoneBook.add("Karpacheva", "89653779071");
-        phoneBook.add("Baranova", "89031335377");
+        try {
+            System.out.println("1! =  " + Factorial.compute(1));
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
 
-        System.out.println("Номера для Karpacheva: " + phoneBook.get("Karpacheva"));
-        System.out.println("Номера для Baranova: " + phoneBook.get("Baranova"));
-        System.out.println("Номера для Stepankov: " + phoneBook.get("Stepankov"));
-        System.out.println("Номера для Fordring: " + phoneBook.get("Fordring"));
+        try {
+            System.out.println("5! =  " + Factorial.compute(5));
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            System.out.println("-1! =  " + Factorial.compute(-1));
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
 
